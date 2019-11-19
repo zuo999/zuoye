41a30ae9b4c5bd7439953f017e16e821c3318a6b
zuo999 <2696134133@qq.com>
2019-11-19 16:04:37 +0800

    000

--- 1112zy.c
+++ 1112zy.c
@@ -1,9 +1,10 @@
 #include<stdio.h>
 int main(void)
 {
-    int i=1,sum=0;
+    int i,term=1,sum=0;
     for(i=1;i<=5;i++){
-        printf("sum=sum+i!\n");
+        term=term*i;
+       sum=sum+term;
     }
     printf("sum=%d\n",sum);
     return 0;
